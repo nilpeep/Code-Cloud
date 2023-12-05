2540afbdb5f1f114c956d151897ef91e17c920c9
Osman Ali Durna <o_durna@hotmail.com>
2023-12-05 19:52:16 +0100

    last step

--- NBA_Legends/src/components/CardContainer.jsx
+++ NBA_Legends/src/components/CardContainer.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import PlayerCard from './PlayerCard'
+import {data} from '../helper/data.js'
+import CardStyle from "./style/CardContainer.module.css"
 
 const CardContainer = () => {
 
-    
   return (
-    <div>
-        <PlayerCard/>
+    <div className={CardStyle.cardContainer}>
+        {data.map((card, index) => (<PlayerCard key={index} {...card}/>))}
     </div>
   )
 }
--- NBA_Legends/src/components/PlayerCard.jsx
+++ NBA_Legends/src/components/PlayerCard.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import CardStyle from "./style/CardContainer.module.css"
 
-const PlayerCard = () => {
+const PlayerCard = ({name, img, statistics}) => {
+    const [flipped, setFlipped] = useState(false);
+
+    const handleFlip = () => {
+        setFlipped(!flipped);
+    }
+
   return (
-    <div>PlayerCard</div>
+    !flipped ? (
+    <div className={CardStyle.card} onClick={handleFlip}>
+        <img className={CardStyle.img} src={img} alt="" />
+        <h3>{name}</h3>
+    </div>) : (<div className={CardStyle.card} onClick={handleFlip}>
+        {statistics.map((stat) => (<p>🏀 {stat}</p>))}
+    </div>)
   )
 }
 
